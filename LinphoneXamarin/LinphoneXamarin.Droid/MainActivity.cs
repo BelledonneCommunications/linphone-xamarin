6ebcf67695c759c67b6525bfc2d823c44f48bf3a
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2016-11-18 10:41:22 +0100

    Initial commit: Start a linphoneCore in an Android application with only the globalstatechanged callback

---
+++ LinphoneXamarin/LinphoneXamarin.Droid/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace LinphoneXamarin.Droid
+{
+	[Activity (Label = "LinphoneXamarin", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        [DllImport("liblinphone-armeabi-v7a")]
+        static extern void ms_set_jvm_from_env(IntPtr jnienv);
+
+        protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+
+            Java.Lang.JavaSystem.LoadLibrary("bctoolbox-armeabi-v7a");
+            Java.Lang.JavaSystem.LoadLibrary("ortp-armeabi-v7a");
+            Java.Lang.JavaSystem.LoadLibrary("mediastreamer_base-armeabi-v7a");
+            Java.Lang.JavaSystem.LoadLibrary("mediastreamer_voip-armeabi-v7a");
+            Java.Lang.JavaSystem.LoadLibrary("linphone-armeabi-v7a");
+
+            ms_set_jvm_from_env(Android.Runtime.JNIEnv.Handle);
+
+            LoadApplication (new LinphoneXamarin.App ());
+        }
+	}
+}
+
---
+++ LinphoneXamarin/README.txt
@@ -0,0 +1,2 @@
+For platform specifics, please see the README in the matching subproject.
+This project has been created using Xamarin 4.2.1.62 under Visual Studio 2015 on a Windows 10 computer.
