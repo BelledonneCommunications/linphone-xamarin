8b2e4865607c67a48e5620de44844ae40b6e6242
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2016-11-21 14:38:25 +0100

    Updated xamarin sample with android video call with remote and local video windows

--- LinphoneXamarin/LinphoneXamarin.Droid/MainActivity.cs
+++ LinphoneXamarin/LinphoneXamarin.Droid/MainActivity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Threading;
 
 using Android.App;
 using Android.Content.PM;
@@ -8,13 +9,62 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+using Org.Linphone.Mediastream.Video;
+using Org.Linphone.Mediastream.Video.Display;
+
 namespace LinphoneXamarin.Droid
 {
-	[Activity (Label = "LinphoneXamarin", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    [Activity(Label = "LinphoneXamarin", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity,  AndroidVideoWindowImpl.IVideoWindowListener
     {
-        [DllImport("liblinphone-armeabi-v7a")]
+        private const string LIBNAME = "liblinphone-armeabi-v7a";
+        private LinphoneXamarin.App app;
+        private System.Threading.Thread coreIterate;
+        private GL2JNIView view1;
+        private SurfaceView view2;
+        private AndroidVideoWindowImpl androidVideo;
+
+        [DllImport(LIBNAME)]
         static extern void ms_set_jvm_from_env(IntPtr jnienv);
+
+        [DllImport(LIBNAME)]
+        static extern void setAndroidLogHandler();
+
+        [DllImport(LIBNAME)]
+        static extern void setMediastreamerAndroidContext(IntPtr jnienv, IntPtr context);
+
+        [DllImport(LIBNAME)]
+        static extern void linphone_core_iterate(IntPtr lc);
+
+        [DllImport(LIBNAME)]
+        static extern void linphone_core_set_native_video_window_id(IntPtr lc, IntPtr id);
+
+        [DllImport(LIBNAME)]
+        static extern void linphone_core_set_native_preview_window_id(IntPtr lc, IntPtr id);
+
+        [DllImport(LIBNAME)]
+        static extern void linphone_core_enable_video(IntPtr lc, int enable_c, int enable_d);
+
+        [DllImport(LIBNAME)]
+        static extern IntPtr linphone_core_create_call_params(IntPtr lc, IntPtr call);
+
+        [DllImport(LIBNAME)]
+        static extern void linphone_call_params_enable_video(IntPtr callp, int enabled);
+
+        [DllImport(LIBNAME)]
+        static extern IntPtr linphone_address_new(string to);
+
+        [DllImport(LIBNAME)]
+        static extern IntPtr linphone_core_invite_address_with_params(IntPtr lc, IntPtr to, IntPtr callp);
+
+        void LinphoneCoreIterate()
+        {
+            while (true)
+            {
+                RunOnUiThread(() => linphone_core_iterate(app.corePtr));
+                System.Threading.Thread.Sleep(50);
+            }
+        }
 
         protected override void OnCreate (Bundle bundle)
 		{
@@ -29,9 +79,63 @@ namespace LinphoneXamarin.Droid
             Java.Lang.JavaSystem.LoadLibrary("linphone-armeabi-v7a");
 
             ms_set_jvm_from_env(Android.Runtime.JNIEnv.Handle);
+            setAndroidLogHandler();
+            setMediastreamerAndroidContext(Android.Runtime.JNIEnv.Handle, this.Handle);
 
-            LoadApplication (new LinphoneXamarin.App ());
+            app = new LinphoneXamarin.App();
+            LoadApplication(app);
+
+            coreIterate = new Thread(LinphoneCoreIterate);
+            coreIterate.IsBackground = false;
+            coreIterate.Start();
+
+            view1 = new GL2JNIView(this);
+            view1.LayoutParameters = new LinearLayout.LayoutParams(500, 500);
+            view2 = new SurfaceView(this);
+            view2.LayoutParameters = new LinearLayout.LayoutParams(500, 500);
+            LinearLayout ll = new LinearLayout(this);
+            ll.Orientation = Orientation.Vertical;
+            ll.LayoutParameters = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.MatchParent);
+            ll.AddView(view1);
+            ll.AddView(view2);
+            SetContentView(ll);
+            view1.SetZOrderOnTop(false);
+            view2.SetZOrderOnTop(true);
+
+            androidVideo = new AndroidVideoWindowImpl(view1, view2, this);
         }
-	}
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            linphone_core_enable_video(app.corePtr, 1, 1);
+            IntPtr callp = linphone_core_create_call_params(app.corePtr, IntPtr.Zero);
+            linphone_call_params_enable_video(callp, 1);
+            IntPtr to = linphone_address_new("sip:sylvain@sip.linphone.org");
+            linphone_core_invite_address_with_params(app.corePtr, to, callp);
+            linphone_core_set_native_video_window_id(app.corePtr, androidVideo.Handle);
+        }
+
+        public void OnVideoPreviewSurfaceDestroyed(AndroidVideoWindowImpl p0)
+        {
+
+        }
+
+        public void OnVideoPreviewSurfaceReady(AndroidVideoWindowImpl p0, SurfaceView p1)
+        {
+            linphone_core_set_native_preview_window_id(app.corePtr, p1.Handle);
+        }
+
+        public void OnVideoRenderingSurfaceDestroyed(AndroidVideoWindowImpl p0)
+        {
+
+        }
+
+        public void OnVideoRenderingSurfaceReady(AndroidVideoWindowImpl p0, SurfaceView p1)
+        {
+            linphone_core_set_native_video_window_id(app.corePtr, p0.Handle);
+        }
+    }
 }
 
--- LinphoneXamarin/LinphoneXamarin/MainPage.xaml.cs
+++ LinphoneXamarin/LinphoneXamarin/MainPage.xaml.cs
@@ -3,95 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Runtime.InteropServices;
 using Xamarin.Forms;
 
 namespace LinphoneXamarin
 {
 	public partial class MainPage : ContentPage
     {
-        const string LIBNAME = "liblinphone-armeabi-v7a";
-
-        struct LinphoneCoreVTable
-        {
-            public IntPtr global_state_changed;
-            public IntPtr registration_state_changed;
-            public IntPtr call_state_changed;
-            public IntPtr notify_presence_received;
-            public IntPtr notify_presence_received_for_uri_or_tel;
-            public IntPtr new_subscription_requested;
-            public IntPtr auth_info_requested;
-            public IntPtr authentication_requested;
-            public IntPtr call_log_updated;
-            public IntPtr message_received;
-            public IntPtr is_composing_received;
-            public IntPtr dtmf_received;
-            public IntPtr refer_received;
-            public IntPtr call_encryption_changed;
-            public IntPtr transfer_state_changed;
-            public IntPtr buddy_info_updated;
-            public IntPtr call_stats_updated;
-            public IntPtr info_received;
-            public IntPtr subscription_state_changed;
-            public IntPtr notify_received;
-            public IntPtr publish_state_changed;
-            public IntPtr configuring_status;
-            public IntPtr display_status;
-            public IntPtr display_message;
-            public IntPtr display_warning;
-            public IntPtr display_url;
-            public IntPtr show;
-            public IntPtr text_received;
-            public IntPtr file_transfer_recv;
-            public IntPtr file_transfer_send;
-            public IntPtr file_transfer_progress_indication;
-            public IntPtr network_reachable;
-            public IntPtr log_collection_upload_state_changed;
-            public IntPtr log_collection_upload_progress_indication;
-            public IntPtr friend_list_created;
-            public IntPtr friend_list_removed;
-        };
-
-        public enum LinphoneGlobalState
-        {
-            LinphoneGlobalOff,
-            LinphoneGlobalStartup,
-            LinphoneGlobalOn,
-            LinphoneGlobalShutdown,
-            LinphoneGlobalConfiguring
-        };
-
-        [DllImport(LIBNAME)]
-        static extern IntPtr linphone_core_v_table_new();
-
-        [DllImport(LIBNAME)]
-        static extern IntPtr linphone_core_new(IntPtr vtable, string config_path, string factory_config_path, IntPtr userdata);
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        delegate void LinphoneCoreGlobalStateChangedCb(IntPtr lc, LinphoneGlobalState state, string message);
-
-        void OnGlobalStateChanged(IntPtr lc, LinphoneGlobalState state, string message)
-        {
-            globalStateLabel.Text += "[LINPHONE GLOBAL STATE CHANGED] " + message + "\r\n";
-        }
-
         public MainPage()
 		{
 			InitializeComponent();
-        }
-
-        void OnButtonClicked(object sender, EventArgs args)
-        {
-            globalStateLabel.Text = "";
-            startLinphoneButton.IsEnabled = false;
-
-            IntPtr vtablePtr = linphone_core_v_table_new();
-            LinphoneCoreVTable vtable = Marshal.PtrToStructure<LinphoneCoreVTable>(vtablePtr);
-            LinphoneCoreGlobalStateChangedCb global_cb = new LinphoneCoreGlobalStateChangedCb(OnGlobalStateChanged);
-            vtable.global_state_changed = Marshal.GetFunctionPointerForDelegate(global_cb);
-            Marshal.StructureToPtr(vtable, vtablePtr, false);
-
-            IntPtr corePtr = linphone_core_new(vtablePtr, null, null, IntPtr.Zero);
         }
     }
 }
