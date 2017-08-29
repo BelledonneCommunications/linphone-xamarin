b413ba51f3ddf9a37cf3c4f7e5a365bbc0677471
Erwan Croze <erwan.croze@belledonne-communications.com>
2017-08-29 11:46:46 +0200

    Adding UWP project

---
+++ LinphoneXamarin/LinphoneXamarin/LinphoneXamarin.Universal/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Xamarin.Forms;
+
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace LinphoneXamarin.Universal
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new LinphoneXamarin.App());
+        }
+    }
+}
